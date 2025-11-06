b77785a336706de079b262a394886ab44921d254
Shuai Shao <sshao0516@gmail.com>
2025-11-07 00:01:04 +0800

    fix

--- custom_components/vaillant_plus/sensor.py
+++ custom_components/vaillant_plus/sensor.py
@@ -203,16 +203,18 @@ class VaillantSensorEntity(VaillantEntity, SensorEntity):
             self._attr_available = value is not None
             # _LOGGER.info("sensor update data %s==%s",self.entity_description.key,value)
             self.async_schedule_update_ha_state(True)
-        if self.entity_description.key.startswith("ext_"):
+            return
+
+        if self.entity_description.key.startswith("ext_") and "burn_status" in data:
             self._attr_available = None
-            if 2 <= data.get("burn_status") <= 17:
+            if 2 <= data.get("burn_status", 0) <= 17:
                 if self.entity_description.key == "ext_CH_flow_temperature":
                     self._attr_native_value = data.get("Flow_temperature")
                     self._attr_available = data.get("Flow_temperature") is not None
                 if self.entity_description.key == "ext_CH_return_temperature":
                     self._attr_native_value = data.get("return_temperature")
                     self._attr_available = data.get("return_temperature") is not None
-            if 22 <= data.get("burn_status") <= 28:
+            if 22 <= data.get("burn_status", 0) <= 28:
                 if self.entity_description.key == "ext_DHW_flow_temperature":
                     self._attr_native_value = data.get("Tank_temperature")
                     self._attr_available = data.get("Tank_temperature") is not None
