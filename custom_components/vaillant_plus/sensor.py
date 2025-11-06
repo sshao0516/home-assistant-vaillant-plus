8991cde72c401a1da91f5fa085cb7cb1eff13418
Shuai Shao <sshao0516@gmail.com>
2025-11-06 18:17:47 +0800

    add some sensors

--- custom_components/vaillant_plus/sensor.py
+++ custom_components/vaillant_plus/sensor.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 
 import logging
+import struct
 from typing import Any
 
 from homeassistant.components.sensor import (
@@ -33,78 +34,20 @@ SENSOR_DESCRIPTIONS = [
         native_unit_of_measurement="bar",
     ),
     SensorEntityDescription(
-        key="indoor_temperature",
-        name="indoor emperature",
-        device_class=SensorDeviceClass.TEMPERATURE,
-        state_class=SensorStateClass.MEASUREMENT,
-        native_unit_of_measurement=UnitOfTemperature.CELSIUS,
+        key="Heating_Curve",
+        name="供暖曲线",
     ),
     SensorEntityDescription(
-        key="Outdoor_Temperature",
-        name="Outdoor Temperature",
-        device_class=SensorDeviceClass.TEMPERATURE,
-        state_class=SensorStateClass.MEASUREMENT,
-        native_unit_of_measurement=UnitOfTemperature.CELSIUS,
-    ),
-    SensorEntityDescription(
-        key="DHW_setpoint",
-        name="生活热水设置温度",
-        device_class=SensorDeviceClass.TEMPERATURE,
-        state_class=SensorStateClass.MEASUREMENT,
-        native_unit_of_measurement=UnitOfTemperature.CELSIUS,
-    ),
-    SensorEntityDescription(
-        key="Lower_Limitation_of_CH_Setpoint",
-        name="暖气最小设置温度",
-        device_class=SensorDeviceClass.TEMPERATURE,
-        state_class=SensorStateClass.MEASUREMENT,
-        native_unit_of_measurement=UnitOfTemperature.CELSIUS,
-    ),
-    SensorEntityDescription(
-        key="Upper_Limitation_of_CH_Setpoint",
-        name="暖气最大设置温度",
-        device_class=SensorDeviceClass.TEMPERATURE,
-        state_class=SensorStateClass.MEASUREMENT,
-        native_unit_of_measurement=UnitOfTemperature.CELSIUS,
-    ),
-    SensorEntityDescription(
-        key="Lower_Limitation_of_DHW_Setpoint",
-        name="生活热水最小设置温度",
-        device_class=SensorDeviceClass.TEMPERATURE,
-        state_class=SensorStateClass.MEASUREMENT,
-        native_unit_of_measurement=UnitOfTemperature.CELSIUS,
-    ),
-    SensorEntityDescription(
-        key="Upper_Limitation_of_DHW_Setpoint",
-        name="生活热水最大设置温度",
-        device_class=SensorDeviceClass.TEMPERATURE,
-        state_class=SensorStateClass.MEASUREMENT,
-        native_unit_of_measurement=UnitOfTemperature.CELSIUS,
-    ),
-    SensorEntityDescription(
-        key="Flow_Temperature_Setpoint",
-        name="暖气设置温度",
-        device_class=SensorDeviceClass.TEMPERATURE,
-        state_class=SensorStateClass.MEASUREMENT,
-        native_unit_of_measurement=UnitOfTemperature.CELSIUS,
-    ),
-    SensorEntityDescription(
-        key="Flow_temperature",
-        name="供暖出水温度",
-        device_class=SensorDeviceClass.TEMPERATURE,
-        state_class=SensorStateClass.MEASUREMENT,
-        native_unit_of_measurement=UnitOfTemperature.CELSIUS,
-    ),
-    SensorEntityDescription(
-        key="return_temperature",
-        name="供暖回水温度",
-        device_class=SensorDeviceClass.TEMPERATURE,
-        state_class=SensorStateClass.MEASUREMENT,
-        native_unit_of_measurement=UnitOfTemperature.CELSIUS,
+        key="Heating_System_Setting",
+        name="暖气系统类型",
     ),
     SensorEntityDescription(
         key="Mode_Setting_CH",
         name="Mode Setting CH",
+    ),
+    SensorEntityDescription(
+        key="Mode_Setting_DHW",
+        name="Mode Setting DHW",
     ),
     SensorEntityDescription(
         key="Heating_System_Setting",
@@ -112,9 +55,77 @@ SENSOR_DESCRIPTIONS = [
     ),
     SensorEntityDescription(
         key="burn_status",
-        name="burn_status",
+        name="burn_status",  # 燃烧状态
+    ),
+    SensorEntityDescription(
+        key="valveModulation_settingValue",
+        name="燃气阀调制设定值",
+    ),
+    SensorEntityDescription(
+        key="valueModulation_currentValue",
+        name="燃气阀调制当前值",
+    ),
+    SensorEntityDescription(
+        key="pump_status",
+        name="pump_status",  # 泵状态
+    ),
+    SensorEntityDescription(
+        key="fan_status",
+        name="风扇状态",
+    ),
+    SensorEntityDescription(
+        key="fan_speed",
+        name="风扇转速",
+    ),
+    SensorEntityDescription(
+        key="maintainence_remainTime",
+        name="保养时间",
     ),
 ]
+
+temperatures = [
+    ("Flow_temperature", "供暖出水温度"),
+    ("return_temperature", "供暖回水温度"),
+    ("Tank_temperature", "水罐温度"),
+    ("Flow_Temperature_Setpoint", "暖气设置温度"),
+    ("indoor_temperature", "室内设定温度"),
+    ("DHW_setpoint", "生活热水设置温度"),
+    ("Lower_Limitation_of_CH_Setpoint", "暖气最小设置温度"),
+    ("Upper_Limitation_of_CH_Setpoint", "暖气最大设置温度"),
+    ("Lower_Limitation_of_DHW_Setpoint", "生活热水最小设置温度"),
+    ("Upper_Limitation_of_DHW_Setpoint", "生活热水最大设置温度"),
+]
+for key, name in temperatures:
+    SENSOR_DESCRIPTIONS.append(
+        SensorEntityDescription(
+            key=key,
+            name=name,
+            device_class=SensorDeviceClass.TEMPERATURE,
+            state_class=SensorStateClass.MEASUREMENT,
+            native_unit_of_measurement=UnitOfTemperature.CELSIUS,
+        )
+    )
+gas_consumption = [
+    ("gas_ch_consumption_today", "暖气燃气消耗-今天"),
+    ("gas_ch_consumption_yesterday", "暖气燃气消耗-昨天"),
+    ("gas_ch_consumption_monthly", "暖气燃气消耗-上个月"),
+    ("gas_ch_consumption_yearly", "暖气燃气消耗-去年"),
+    ("gas_dhw_consumption_today", "生活热水燃气消耗-今天"),
+    ("gas_dhw_consumption_yesterday", "生活热水燃气消耗-昨天"),
+    ("gas_dhw_consumption_monthly", "生活热水燃气消耗-上个月"),
+    ("gas_dhw_consumption_yearly", "生活热水燃气消耗-去年"),
+]
+for key, name in gas_consumption:
+    SENSOR_DESCRIPTIONS.append(
+        SensorEntityDescription(
+            key=key,
+            name=name,
+            device_class=SensorDeviceClass.GAS,
+            state_class=SensorStateClass.MEASUREMENT,
+            # native_unit_of_measurement=UnitOfTemperature.CUBIC_METERS,
+            native_unit_of_measurement="kWh",
+        )
+    )
 
 
 async def async_setup_entry(
@@ -166,12 +177,18 @@ class VaillantSensorEntity(VaillantEntity, SensorEntity):
         """Return a unique ID."""
         return f"{self.device.id}_{self.entity_description.key}"
 
+    def _hex2float32(self, hex_str):
+        # little-endian IEEE 754 float32
+        return struct.unpack(">f", int(hex_str, 16).to_bytes(4, byteorder="little"))[0]
+
     @callback
     def update_from_latest_data(self, data: dict[str, Any]) -> None:
         """Update the entity from the latest data."""
 
         if self.entity_description.key in data:
             value = data.get(self.entity_description.key)
+            if self.entity_description.key.startswith("gas_"):
+                value = self._hex2float32(value)
             self._attr_native_value = value
             self._attr_available = value is not None
             # _LOGGER.info("sensor update data %s==%s",self.entity_description.key,value)
